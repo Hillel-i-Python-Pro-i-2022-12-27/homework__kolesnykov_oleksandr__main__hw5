8ebb798a98693381f6ec98bb3cd6058022e5ee2f
sashkakoleso88 <sashkakoleso88@gmail.com>
2023-01-20 09:31:12 +0200

    add "text data" and "files_output" + constants (PATH_TO_TEXT_DATA, PATH_TO_FILES_OUTPUT)

--- application/project_paths/paths.py
+++ application/project_paths/paths.py
@@ -3,3 +3,5 @@ from typing import Final
 
 
 ROOT_PATH: Final[Path] = Path(__file__).parents[2].absolute()
+PATH_TO_TEXT_DATA: Final[Path] = ROOT_PATH.joinpath("text_data")
+PATH_TO_FILES_OUTPUT: Final[Path] = ROOT_PATH.joinpath("files_output")
