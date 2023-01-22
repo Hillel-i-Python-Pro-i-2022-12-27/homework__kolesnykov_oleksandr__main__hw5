710317828d9417c8d745f8223d1ae3f0522e2a6d
sashkakoleso88 <sashkakoleso88@gmail.com>
2023-01-22 12:34:31 +0200

    refactored imports in "main.py" + refactored module "work_with_files" -> deleted "read_file" and "write_file", added "actions"

--- application/work_with_files/actions.py
+++ application/work_with_files/actions.py
@@ -1,3 +1,10 @@
+def read_file(path_to_file):
+
+    with open(path_to_file) as file:
+        print(f"Читаю содержимое файла {path_to_file}...")
+        return file.read()
+
+
 def write_data_in_file(data, file):
 
     with open(data) as opened_file:
