57091282573be43147f9704e785210a4536a9f73
sashkakoleso88 <sashkakoleso88@gmail.com>
2023-01-23 04:19:38 +0200

    add new function "create_new_txt_file", refactored "main.py"

--- application/work_with_files/actions.py
+++ application/work_with_files/actions.py
@@ -1,4 +1,18 @@
 from application.logging.loggers import get_core_logger
+from application.project_paths.paths import PATH_TO_FILES_INPUT
+
+
+def create_new_txt_file(name="test.txt"):
+
+    path_to_new_file = PATH_TO_FILES_INPUT.joinpath(name)
+    create_logger = get_core_logger("create_logger")
+    create_logger.info(f'Creating file "{name}" -> {path_to_new_file}')
+    content = "Greetings ! I am a file with useless information"
+
+    with open(path_to_new_file, "w", encoding="UTF-8") as new_file:
+
+        new_file.writelines(content)
+        print(f"Successfully ! File {name} is ready\n")
 
 
 def read_file(path_to_file):
@@ -19,7 +33,7 @@ def write_data_in_file(data, file):
 
         strings = opened_file.readlines()
 
-        with open(file, "w") as file_to_write:
+        with open(file, "w", encoding="UTF-8") as file_to_write:
 
             write_logger = get_core_logger("write_logger")
             write_logger.info(f"Writing data to file {file}")
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from application.find_average.find_average import find_average_weight, find_aver
 from application.logging.init_logging import init_logging
 from application.project_paths.paths import PATH_TO_FILES_INPUT, PATH_TO_FILES_OUTPUT
 from application.users_generator.users_generator import create_users_generator, get_random_users_email
-from application.work_with_files.actions import read_file, write_data_in_file
+from application.work_with_files.actions import read_file, write_data_in_file, create_new_txt_file
 
 
 def main_func():
 
-    print(read_file(PATH_TO_FILES_INPUT.joinpath("example.txt")) + "\n")
-    write_data_in_file(PATH_TO_FILES_INPUT.joinpath("example.txt"), PATH_TO_FILES_OUTPUT.joinpath("recorded_data.txt"))
-    generator = create_users_generator(5)  # создастся генератор 10ти случайных имён
+    create_new_txt_file("text_data.txt")
+    print(read_file(PATH_TO_FILES_INPUT.joinpath("text_data.txt")) + "\n")
+    write_data_in_file(
+        PATH_TO_FILES_INPUT.joinpath("text_data.txt"), PATH_TO_FILES_OUTPUT.joinpath("recorded_data.txt")
+    )
+    generator = create_users_generator(10)  # создастся генератор 10ти случайных имён
     print(*get_random_users_email(generator), sep="\n")  # распечатается генератор 10 случайных имейлов
     print()
     print(astronauts_info.get_info())
